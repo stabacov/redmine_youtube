e09b4def9a856543af546ed8f0cefcd3eb35782e
Tatsuhito Kato <stabacov@gmail.com>
2013-02-06 13:31:57 +0900

    Compatible with Redmine 2.2

--- init.rb
+++ init.rb
@@ -1,12 +1,12 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_youtube do
-	name 'Redmine Youtube wiki macro plugin'
-	author 'J.W.Marsden'
+	name 'Redmine Youtube wiki macro plugin (modified)'
+	author 'Tatsuhito Kato (original by J.W.Marsden)'
 	description 'This plugin adds a youtube macro to Redmine that allows the posting of YouTube videos.'
-	version '0.0.1'
-	url 'http://jwm.slavitica.net/p/projects/redmine-youtube'
-	author_url 'http://jwm.slavitica.net'
+	version '0.0.2'
+#	url 'http://jwm.slavitica.net/p/projects/redmine-youtube'
+#	author_url 'http://jwm.slavitica.net'
 	
 	Redmine::WikiFormatting::Macros.register do
 		desc = "Redmine Youtube Macro! The correct usage is {{youtube(&lt;video key&gt;,[&lt;width&gt;,&lt;height&gt;])}}. eg {{youtube(4N3N1MlvVc4)}} or {{youtube(4N3N1MlvVc4,800,600)}}</pre>"
@@ -22,11 +22,9 @@ Redmine::Plugin.register :redmine_youtube do
 					w = args[1]
 					h = args[2]
 				end
-				out = <<"EOF"
-<iframe width="#{w}" height="#{h}" src="http://www.youtube.com/embed/#{v}" frameborder="0" allowfullscreen></iframe>
-EOF
+				content_tag(:iframe, '', :width => w, :height => h, :src => "http://www.youtube.com/embed/#{v}", :frameborder => 0, :allowfullscreen => :allowfullscreen)
 			else
-				out = "<pre>Error in youtube macro. The correct usage is {{youtube(&lt;video key&gt;,[&lt;width&gt;,&lt;height&gt;])}}. \r\neg {{youtube(4N3N1MlvVc4)}} or {{youtube(4N3N1MlvVc4,800,600)}}</pre>"
+				content_tag(:pre, "Error in youtube macro. The correct usage is {{youtube(<video key>,[<width>,<height>])}}. \neg {{youtube(4N3N1MlvVc4)}} or {{youtube(4N3N1MlvVc4,800,600)}}")
 			end
 
 		end
